018a41db8dd28a95ed011fd8a2fa14b10381c4ea
Janne Härkönen <jth@l228.(none)>
2010-04-28 23:14:07 +0300

    initial source for swing & db demo

---
+++ src/dbapp.clj
@@ -0,0 +1,36 @@
+(ns dbapp
+    (:gen-class))
+
+(import '(javax.swing JFrame JLabel JTextField JButton JList)
+        '(java.awt.event ActionListener)
+        '(java.awt GridLayout))
+
+(defn store [username userlist]
+  (.setListData userlist (to-array [username])))
+
+(defn store-action [username-input userlist]
+  (proxy [ActionListener] []
+    (actionPerformed [evt]
+      (let [username (.getText username-input)]
+        (store username userlist)))))
+
+(defn sut []
+  (let [frame (JFrame. "Database Application")
+        username-input (JTextField.)
+        username-label (JLabel. "Username")
+        submit-button (JButton. "Add User")
+        userlist-label (JLabel. "Users")
+        userlist (JList.)]
+    (.addActionListener submit-button (store-action username-input userlist))
+    (doto frame
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setLayout (GridLayout. 2 2 3 3))
+      (.add username-label)
+      (.add username-input)
+      (.add submit-button)
+      (.add userlist)
+      (.setSize 300 80)
+      (.setVisible true))))
+
+(defn -main [& args]
+    (sut))
