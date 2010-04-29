db6e4e5cc73dfe59561a478df59710387d3cad21
Janne Härkönen <jth@l228.(none)>
2010-04-29 17:21:54 +0300

    Added hsqldb dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject dbapp "0.1"
     :dependencies [[org.clojure/clojure "1.1.0"]
-                   [org.clojure/clojure-contrib "1.1.0"]]
+                   [org.clojure/clojure-contrib "1.1.0"]
+                   [hsqldb/hsqldb "1.8.0.10"]]
     :main dbapp)
