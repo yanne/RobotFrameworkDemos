da2a64fa784ac8ef5e5cea38b138b886f1a38bc9
Kari Husa <kari.husa@reaktor.fi>
2010-05-02 11:10:11 +0300

    Newer Clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dbapp "0.1"
-    :dependencies [[org.clojure/clojure "1.1.0"]
-                   [org.clojure/clojure-contrib "1.1.0"]
-                   [hsqldb/hsqldb "1.8.0.10"]]
-    :main dbapp)
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [hsqldb/hsqldb "1.8.0.10"]]
+  :main dbapp)
