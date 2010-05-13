38cb9bdbbd2d407b4394a1d813eb04c1fbda07d4
Janne Härkönen <jth@reaktor.fi>
2010-05-13 16:26:24 +0300

    Do the layout a bit better

--- src/dbapp.clj
+++ src/dbapp.clj
@@ -1,9 +1,9 @@
 (ns dbapp
     (:gen-class))
 (use 'task-store)
-(import '(javax.swing JFrame JLabel JTextField JButton JList)
+(import '(javax.swing JFrame JLabel JTextField JButton JList JPanel BoxLayout)
         '(java.awt.event ActionListener)
-        '(java.awt FlowLayout))
+        '(java.awt BorderLayout))
 
 (defn refresh-tasklist [tasklist]
   (.setListData tasklist (to-array (map #(:name %) (all-tasks)))))
@@ -24,6 +24,7 @@
 
 (defn sut []
   (let [frame (JFrame. "TODO List")
+        name-panel (JPanel.)
         task-input (JTextField. 20)
         task-label (JLabel. "Task: ")
         submit-button (JButton. "Add Task")
@@ -32,13 +33,16 @@
         tasklist (JList.)]
     (.addActionListener submit-button (store-action task-input tasklist))
     (.addActionListener delete-button (del-action task-input tasklist))
-    (doto frame
-      (.setLayout (FlowLayout.))
+    (doto name-panel
+      (.setLayout (BoxLayout. name-panel BoxLayout/X_AXIS))
       (.add task-label)
       (.add task-input)
-      (.add submit-button)
-      (.add delete-button)
-      (.add tasklist)
+      (.add submit-button))
+    (doto frame
+      (.setLayout (BorderLayout.))
+      (.add name-panel BorderLayout/NORTH)
+      (.add tasklist BorderLayout/CENTER)
+      (.add delete-button BorderLayout/SOUTH)
       (.setSize 400 300)
       (.setVisible true))
     (refresh-tasklist tasklist)
