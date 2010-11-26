9bad0f7a5ea63f9469bba2e08d9b51283bef9889
Janne Härkönen <jth@reaktor.fi>
2010-11-26 22:58:16 +0200

    project.clj: specify jar target dir

--- todo-list/project.clj
+++ todo-list/project.clj
@@ -1,5 +1,6 @@
-(defproject dbapp "0.1"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+(defproject todo-application "0.1"
+  :dependencies [[org.clojure/clojure "1.2.0"] 
                  [org.clojure/clojure-contrib "1.2.0"]
                  [hsqldb/hsqldb "1.8.0.10"]]
+  :jar-dir "target"
   :main TodoApplication)
