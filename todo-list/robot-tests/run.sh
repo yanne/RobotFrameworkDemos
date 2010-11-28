55bd31e61cd515b6332fbc7fa6fe7927f1845d62
Janne Härkönen <jth@reaktor.fi>
2010-11-28 16:23:24 +0200

    testrunner: extract CP variable

--- todo-list/robot-tests/run.sh
+++ todo-list/robot-tests/run.sh
@@ -1,1 +1,2 @@
-CLASSPATH=robot-tests/testlibs/swinglibrary-1.1.1.jar:robot-tests/testlibs/dblibrary-1.0.jar:target/todo-application-0.1-standalone.jar jybot -d /tmp/ robot-tests/
+CP=robot-tests/testlibs/swinglibrary-1.1.1.jar:robot-tests/testlibs/dblibrary-1.0.jar:target/todo-application-0.1-standalone.jar
+CLASSPATH=$CP jybot -d /tmp/ robot-tests/
